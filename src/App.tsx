b74e1af5514fbcd49d7a9556062756177eb90880
willmorim <pdbawilliam@gmail.com>
2020-10-13 23:08:05 -0300

    Criando paginas de home e de mapa e estilos

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 
+import './styles/global.css';
+
+import Routes from './routes';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Happy</h1>
-    </div>
-  );
+  return <Routes />;
 }
 
 export default App;
